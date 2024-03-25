dfd0bfc9cb2c72c415e75c44ed0ec2fed9dbd304
zhan2511 <3284063926@qq.com>
2024-03-25 15:48:10 +0800

    new files

---
+++ mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->stackedWidget->setCurrentIndex(0);
+
+    connect(ui->Start,&QPushButton::clicked,this,[=](){
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+
+    connect(ui->Back,&QPushButton::clicked,this,[=](){
+        ui->stackedWidget->setCurrentIndex(0);
+    });
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
